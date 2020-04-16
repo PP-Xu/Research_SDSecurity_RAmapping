d36cebcd5d44c4d04a7c003e25d2a1768c0ae34a
PP-Xu <63767275+PP-Xu@users.noreply.github.com>
2020-04-16 12:25:06 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python
+
 # -*- coding:utf-8 -*-
+
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column # 创建字段用
-from sqlalchemy import Integer, String, DECIMAL # 导入对应的字段类型
-from sqlalchemy import ForeignKey  #导入外键
-from sqlalchemy import create_engine # 配置引擎
-from sqlalchemy.orm import  relationship  #创建关系
+from sqlalchemy import Column 
+from sqlalchemy import Integer, String, DECIMAL 
+from sqlalchemy import ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.orm import  relationship 
 
 
-# 使用基类模型进行创建新的类,这个新的类就有了与表对应的结构映射关系
+# the Base is uesd to creat other new classes with mapping relations
 Base = declarative_base()
-# 生成自己的类,类名就是表名
-#需求
+# creat new class
+
+#requirements
 class Requirement(Base):
     __tablename__='requirement'
     id = Column(Integer, primary_key=True)
@@ -21,7 +24,7 @@ class Requirement(Base):
     def __repr__(self):
         return self.title
 
-#防御
+#Defense
 class Defense(Base):
     __tablename__='defense'
     id = Column(Integer, primary_key=True)
@@ -36,14 +39,15 @@ class Defense(Base):
     def getInfo(self):
         return self.requirement.title+"--"+self.title
 
-#方法
+#Activities
 class SecurityActivities(Base):
     __tablename__='securityActivities'
     id = Column(Integer, primary_key=True)
     title = Column('title',String(255),nullable=False)
-    cost = Column('cost',Integer,nullable=False) #花费金额
-    maintenance = Column('maintenance',Integer,nullable=False) #维护等级难度
-    efficiency = Column('efficiency',Integer,nullable=False) #有效程度
+    
+    cost = Column('cost',Integer,nullable=False) 
+    maintenance = Column('maintenance',Integer,nullable=False)
+    efficiency = Column('efficiency',Integer,nullable=False)
     defense_id = Column(Integer,ForeignKey("defense.id"),nullable=False)
     defense = relationship("Defense",order_by="Defense.id",back_populates="securityActivities")
 
@@ -59,11 +63,11 @@ class SecurityActivities(Base):
 
 
 
-# 连接数据库并建立表格
+# connect the database
 def link_mysql():
-    # 配置引擎
+    # config the engine
     engine = create_engine('mysql+pymysql://root:password@127.0.0.1:3306/test')
-    # 利用基类创建表
+    # cerat tables
     Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
